c5bffed90e47c139e9158da6777ab7b0f2fed972
Glitch (dandy-dorian-palladium) <none>
2020-04-08 15:23:45 +0000

    📙🌘 Updated with Glitch

---
+++ client/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import NexmoApp from './NexmoApp';
+
+ReactDOM.render(<NexmoApp />, document.querySelector('#root'));
